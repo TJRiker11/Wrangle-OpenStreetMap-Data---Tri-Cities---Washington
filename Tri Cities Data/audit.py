9e431196bef8c9dc6f41cd16e0a16e885f388690
TrikerDev <65632351+TrikerDev@users.noreply.github.com>
2020-06-13 13:43:58 -0700

    Update audit.py

--- Tri Cities Data/audit.py
+++ Tri Cities Data/audit.py
@@ -90,3 +90,72 @@ for st_type, ways in st_types.items():
     for name in ways:
         better_name = update_name(name, mapping)
         print (name, "=>", better_name)
+
+            
+            
+            
+            
+ zip_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+zip_types = defaultdict(set)
+
+expected_zip = {}
+
+def audit_zip_codes(zip_types, zip_name, regex, expected_zip):
+    m = regex.search(zip_name)
+    if m:
+        zip_type = m.group()
+        if zip_type not in expected_zip:
+             zip_types[zip_type].add(zip_name)
+
+def is_zip_name(elem):
+    return (elem.attrib['k'] == "addr:postcode")
+
+
+def audit_zip(filename, regex):
+    for event, elem in ET.iterparse(filename, events=("start",)):
+        if elem.tag == "way" or elem.tag == "node":
+            for tag in elem.iter("tag"):
+                if is_zip_name(tag):
+                    audit_zip_codes(zip_types, tag.attrib['v'], regex, expected_zip)
+    pprint.pprint(dict(zip_types))
+
+
+    
+audit_zip(OSM_FILE, zip_type_re)
+
+
+for zip_type, ways in zip_types.iteritems(): 
+        for name in ways:
+            if "-" in name:
+                name = name.split("-")[0].strip()
+            elif len(str(name))>5:
+                name=name[0:5]
+                # This function because one of the postcodes in the dataset has only 4 digits. This is basically setting
+                # it to 'unknown' as its all zeros
+            elif len(str(name))<5:
+                name = '00000'
+            print name
+            
+            
+            
+ # The way to audit the postcodes is to make them all uniform. Since the vast majority of them are 5 digits, we will be
+# auditing the longer codes by stripping off the extra digits and he '-'. This will make every post code a uniform 5 digits
+def update_postcode(name): 
+    if "-" in name:
+        name = name.split("-")[0].strip()
+    elif len(str(name))>5:
+        name=name[0:5]
+    elif len(str(name))<5:
+        name = '00000'
+    return name
+
+
+
+def audit_postcode_tag(element,regex=re.compile(r'\b\S+\.?$', re.IGNORECASE)):
+    post_code=element.get('v')
+    m = regex.search(post_code)
+    if m:
+        better_postcode=update_postcode(post_code)
+        return better_postcode
+    return post_code
